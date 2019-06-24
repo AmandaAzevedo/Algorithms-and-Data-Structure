6b178f2edc970835c8b490f9f939eb41309dbe8d
Amanda Azevedo <amanda.azevedo@dce.ufpb.br>
2019-06-24 19:55:59 -0300

    algoritmo de ordenação - insertion sort não comentado

--- Algoritmos-de-ordenção/insertionSort/InsertionSort.java
+++ Algoritmos-de-ordenção/insertionSort/InsertionSort.java
@@ -2,4 +2,21 @@ package insertionSort;
 
 public class InsertionSort {
 
+	public void insertionSort(Comparable[] vetorDesordenado) {
+
+		Comparable chave;
+		int i;
+
+		for (int j = 0; j < vetorDesordenado.length; j++) {
+			chave = vetorDesordenado[j];
+			i = j - 1;
+
+			while (i >= 0 && vetorDesordenado[i].compareTo(chave) > 0) {
+				vetorDesordenado[i + 1] = vetorDesordenado[i];
+				i = i - 1;
+			}
+
+			vetorDesordenado[i + 1] = chave;
+		}
+	}
 }
--- Algoritmos-de-ordenção/insertionSort/TesteInsertion.java
+++ Algoritmos-de-ordenção/insertionSort/TesteInsertion.java
@@ -2,4 +2,24 @@ package insertionSort;
 
 public class TesteInsertion {
 
+	public static void main(String[] args) {
+
+		InsertionSort ordenador = new InsertionSort();
+
+		Comparable[] vetorDesordenado = new Comparable[5];
+
+		vetorDesordenado[0] = 4;
+		vetorDesordenado[1] = 3;
+		vetorDesordenado[2] = 2;
+		vetorDesordenado[3] = 3;
+		vetorDesordenado[4] = 1;
+
+		ordenador.insertionSort(vetorDesordenado);
+
+		for (int k = 0; k < vetorDesordenado.length; k++) {
+			System.out.println(vetorDesordenado[k]);
+		}
+
+	}
+
 }
