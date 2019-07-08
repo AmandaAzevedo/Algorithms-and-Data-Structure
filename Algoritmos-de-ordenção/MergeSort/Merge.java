bcabb980625e69e7091ec444abbac9dc17e328b4
Amanda Azevedo Martins <amanda.azevedo@labs.dcx>
2019-07-08 09:42:22 -0300

    Algoritmo merge sort completo

---
+++ Algoritmos-de-ordenção/MergeSort/Merge.java
@@ -0,0 +1,81 @@
+
+public class Merge {
+
+	private Merge() {
+	}
+
+	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+
+		assert isSorted(a, lo, mid);
+		assert isSorted(a, mid + 1, hi);
+
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = a[k];
+		}
+
+		int i = lo, j = mid + 1;
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid)
+				a[k] = aux[j++];
+			else if (j > hi)
+				a[k] = aux[i++];
+			else if (less(aux[j], aux[i]))
+				a[k] = aux[j++];
+			else
+				a[k] = aux[i++];
+		}
+
+		assert isSorted(a, lo, hi);
+	}
+
+	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+		if (hi <= lo)
+			return;
+		int mid = lo + (hi - lo) / 2;
+		sort(a, aux, lo, mid);
+		sort(a, aux, mid + 1, hi);
+		merge(a, aux, lo, mid, hi);
+	}
+
+	public static void sort(Comparable[] a) {
+		Comparable[] aux = new Comparable[a.length];
+		sort(a, aux, 0, a.length - 1);
+		assert isSorted(a);
+	}
+
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	private static void exch(Object[] a, int i, int j) {
+		Object swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.println(a[i]);
+		}
+
+	}
+
+	private static boolean isSorted(Comparable[] a) {
+		return isSorted(a, 0, a.length - 1);
+	}
+
+	private static boolean isSorted(Comparable[] a, int lo, int hi) {
+		for (int i = lo + 1; i <= hi; i++)
+			if (less(a[i], a[i - 1]))
+				return false;
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String[] a = StdIn.readAllStrings();
+		Merge.sort(a);
+		show(a);
+
+	}
+
+}
